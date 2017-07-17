82ceaa93e051b6d512273a8b8d9a9bcb70da3afe
Mary Ashton Inglis <maryashton.inglis@gmail.com>
2017-07-17 12:03:37 -0400

    exercise set up

--- server-pure-node.js
+++ server-pure-node.js
@@ -7,24 +7,27 @@ var connectionString = "pg://maryashton@localhost:5432/puppies";
 var client = new pg.Client(connectionString);
 client.connect();
 
-http.createServer(function (req, res) {
+var query = client.query("SELECT * from pups ORDER BY id ASC");
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on("end", function (result) {
+    console.log(JSON.stringify(result.rows, null, "    "));
+    client.end();
+});
 
-  res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
-  var purl=url.parse(req.url,true);
 
-  if(purl.pathname=='/test')
-  	res.end('Test');
-  else
-	var query = client.query("SELECT * from pups ORDER BY id ASC");
-	query.on("row", function (row, result) {
-	    result.addRow(row);
-	});
-	query.on("end", function (result) {
-	    console.log(JSON.stringify(result.rows, null, "    "));
-	    res.end(JSON.stringify(result.rows));
-	    // client.end();
-	});
+// UNCOMMENT BLOCK OUT BELOW
+// THEN ADD ELSE CLAUSE THAT RETURNS THE ABOVE QUERY
+
+// http.createServer(function (req, res) {
+
+//   res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
+//   var purl=url.parse(req.url,true);
+
+//   if(purl.pathname=='/test')
+//   	res.end('Test');
   
-}).listen(3111);
-console.log('Server running at http://127.0.0.1:3111/');
+// }).listen(3111);
+// console.log('Server running at http://127.0.0.1:3111/');
 
